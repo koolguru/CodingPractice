f0170bba4e29edbbe2602c79ca147741cde20b0c
koolguru <koolguru2@gmail.com>
2018-08-13 12:42:51 -0700

    Solved LC# 515 Largest Value in each tree row

--- Leetcode/LargestValueFromTree.cpp
+++ Leetcode/LargestValueFromTree.cpp
@@ -1,3 +1,4 @@
+// Solution for LC # 515: Find the largest value in each tree row
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -12,58 +13,32 @@ public:
 
     vector<int> largestValues(TreeNode* root) {
         vector<int> results;
-        if(root == NULL)
-        {
+        if(root == NULL) {
             return results;
         }
         queue<TreeNode*> queue;
-        int comparisonsRemaining = 0;
-        int currentMax = -2147483648;
-
-        if(root->left != NULL)
-        {
-            queue.push(root->left);
-            comparisonsRemaining++;
-        }
-        if(root->right != NULL)
-        {
-            queue.push(root->right);
-            comparisonsRemaining++;
-        }
-        results.push_back(root->val);
-        if(queue.empty())
-        {
-            return results;
-        }
-
-        while(!queue.empty())
-        {
-            if(comparisonsRemaining == 0)
-            {
-                comparisonsRemaining = queue.size();
+        queue.push(root);
+        int levelSize = 1;
+        int currentMax = std::numeric_limits<int>::min();;
+        while(!queue.empty()) {
+            TreeNode* front = queue.front();
+            queue.pop();
+            levelSize--;
+            if(front->left != NULL) {
+                queue.push(front->left);
+            }
+            if(front->right != NULL) {
+                queue.push(front->right);
+            }
+            if(front->val > currentMax) {
+                currentMax = front->val;
+            }
+            if(levelSize == 0) {
+                levelSize = queue.size();
                 results.push_back(currentMax);
-                currentMax = -2147483648;
-            }
-            else
-            {
-                root = queue.front();
-                if(root->val > currentMax)
-                {
-                    currentMax = root->val;
-                }
-                comparisonsRemaining--;
-                if(root->left != NULL)
-                {
-                    queue.push(root->left);
-                }
-                if(root->right != NULL)
-                {
-                    queue.push(root->right);
-                }
-                queue.pop();
+                currentMax = std::numeric_limits<int>::min();;
             }
         }
-        results.push_back(currentMax);
         return results;
     }
 };
